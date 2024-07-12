7bb99f1a5cbd0db4f2cd74d5707dec54e9caf317
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-07-12 14:36:47 +0100

    update to create put endPointe for both check in and out

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 const getNameStatus = async (req, res) => {
-  const query = "select * from Entery_data";
+  const query = "select * from Entry_data";
   try {
     const data = await pool.query(query);
     res.status(200).json(data.rows);
@@ -34,8 +34,55 @@ const getNameStatus = async (req, res) => {
     pool.end();
   }
 };
-
 module.exports = getNameStatus;
+
+app.put('/checkIn', async (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: 'Name is required' });
+  }
+const query = `UPDATE Entry_data
+                  SET status = 'in'
+                  WHERE name = $1 AND status = 'out'
+                  RETURNING *`
+  try {
+    const result = await pool.query(query, [name]);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: 'No matching entry found or already checked in' });
+    }
+
+    res.json({ message: 'Status updated to in', entry: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.put('/checkOut', async (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: 'Name is required' });
+  }
+const query = `UPDATE Entry_data
+                  SET status = 'out'
+                  WHERE name = $1 AND status = 'in'
+                  RETURNING *`
+  try {
+    const result = await pool.query(query, [name]);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: 'No matching entry found or already checked in' });
+    }
+
+    res.json({ message: 'Status updated to in', entry: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 
 
