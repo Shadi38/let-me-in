e8a1f1caba54f249c7ccaffc7e6f464aa5ea508d
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-07-12 12:36:48 +0100

    create database and successful make get request

--- server/server.js
+++ server/server.js
@@ -1,17 +1,44 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
 const cors = require("cors");
 const port = process.env.PORT || 3003;
-// const pool = require("./database/db");
+const pool = require("./database/data");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-// pool.connect();
+pool.connect();
 
 app.listen(port, () => {
   console.log(`Server is running on Port: ${port}`);
 });
+
+app.get("/", (req, res) => {
+  res.send(`Welcome to Final Project server on port  ${port}`);
+});
+
+const getNameStatus = async (req, res) => {
+  const query = "select * from Entery_data";
+  try {
+    const data = await pool.query(query);
+    res.status(200).json(data.rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      result: "failure",
+      message: "No data found",
+    });
+    pool.end();
+  }
+};
+
+module.exports = getNameStatus;
+
+
+
+
+app.get("/names", getNameStatus);
+
