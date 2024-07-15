580af8a22dee2a3a7dd716113164e0ba89b50578
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-07-15 18:28:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import React, { useState } from "react";
 import "./App.css";
 import Buttons from "./components/Buttons";
 import InputName from "./components/ListName";
 
-
 function App() {
+  const [selectedName, setSelectedName] = useState("");
+
   return (
     <div className="App">
-      <h1>let me in</h1>
-      
-       <InputName/>
-      <Buttons />
+      <h1>Let me in</h1>
+      <InputName setSelectedName={setSelectedName} />
+      <Buttons name={selectedName} />
     </div>
   );
 }
